39c4e9ea5e85b6e9ced84275dba2756082f9374a
BACK-2-15 <back-2-15@Erxess-Mac-mini.local>
2026-02-02 20:41:21 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+interface IMovie {
+  title: string;
+}
+
 function App() {
-  const [count, setCount] = useState<number>(0);
+  const [movies, setMovies] = useState<IMovie[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/movie/getMovies")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setLoading(false);
+        setMovies(data);
+      });
+  }, []);
+
+  if (loading) {
+    return <h1>Unshij bn</h1>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <p>count is {count}</p>
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <button onClick={() => setCount(count - 1)}>-</button>
-        <button onClick={() => setCount(0)}>Reset</button>
-      </div>
-
-      <NameAdder />
-
-      <RevealSecret />
-    </>
-  );
-}
-
-function NameAdder() {
-  const [name, setName] = useState<string>("");
-
-  return (
-    <>
-      <input
-        type="text"
-        placeholder="Enter your name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <p>Hello! {name}</p>
-    </>
-  );
-}
-
-function RevealSecret() {
-  const [secret, setSecret] = useState<string>("Reveal Secret");
-
-  return (
-    <>
-      <button
-        onClick={() => setSecret("Nice job")}
-        onDoubleClickCapture={() => setSecret("Click again to reveal")}
-      >
-        {secret}
-      </button>
+      {movies.map((movie) => {
+        return <h1>{movie.title}</h1>;
+      })}
     </>
   );
 }
