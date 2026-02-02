ac9cfe13f4d3d70c59fa4cceddd9fd0e21ab83c0
BACK-2-15 <back-2-15@Erxess-Mac-mini.local>
2026-02-02 18:46:32 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-type UserProfileProps = {
-  name: string;
-  setName: React.Dispatch<React.SetStateAction<string>>;
-  age: number;
-  setAge: React.Dispatch<React.SetStateAction<number>>;
-};
-
 function App() {
   const [count, setCount] = useState<number>(0);
-
-  const [userName, setUserName] = useState<string>("");
-  const [userAge, setUserAge] = useState<number>(0);
 
   return (
     <>
@@ -39,13 +29,6 @@ function App() {
       <NameAdder />
 
       <RevealSecret />
-
-      <UserProfile
-        name={userName}
-        setName={setUserName}
-        age={userAge}
-        setAge={setUserAge}
-      />
     </>
   );
 }
@@ -77,49 +60,7 @@ function RevealSecret() {
       >
         {secret}
       </button>
-      <p>Click on the button to reveal secret</p>
     </>
-  );
-}
-
-function UserProfile({ name, setName, age, setAge }: UserProfileProps) {
-  const [isActive, setIsActive] = useState<boolean>(false);
-
-  const containerStyle: React.CSSProperties = {
-    padding: "10px",
-    borderRadius: "8px",
-    border: "1px solid #ccc",
-    marginTop: "10px",
-    display: "inline-block",
-    boxShadow: isActive ? "0 0 15px 3px green" : "none",
-    transition: "box-shadow 0.3s ease",
-  };
-
-  return (
-    <div style={containerStyle}>
-      <input
-        type="text"
-        placeholder="Enter your name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <p>Your age is: {age}</p>
-      <div>
-        <button onClick={() => setAge(age + 1)}>+</button>
-        <button onClick={() => setAge(age - 1)}>-</button>
-        <button onClick={() => setAge(0)}>Reset</button>
-      </div>
-      <div style={{ marginTop: "10px" }}>
-        <label>
-          <input
-            type="checkbox"
-            checked={isActive}
-            onChange={(e) => setIsActive(e.target.checked)}
-          />{" "}
-          Is Active
-        </label>
-      </div>
-    </div>
   );
 }
 
